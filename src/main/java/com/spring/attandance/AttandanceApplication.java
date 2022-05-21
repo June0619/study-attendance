f84caed7f6d7f54ec0a07d1344884814f3b80dcb
June0619 <jjw5050@hotmail.com>
2022-05-21 09:54:47 +0900

    wip: 스켈레톤 구현 중
    1. 출석부 어플리케이션 스켈레톤 구현 중

---
+++ src/main/java/com/spring/attandance/AttandanceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.attandance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttandanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttandanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/attandance/AttandanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.attandance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttandanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
