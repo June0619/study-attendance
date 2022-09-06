edb32f16b2b71f3328703f456000f39e30cdb1a8
June0619 <jjw5050@hotmail.com>
2022-09-06 18:19:58 +0900

    wip: validator interface 작업 중
    1. 작업 예정 검사 추가

--- src/main/java/com/spring/attandance/service/MemberValidator.java
+++ src/main/java/com/spring/attandance/service/MemberValidator.java
@@ -5,6 +5,15 @@ import com.spring.attandance.domain.Member;
 public interface MemberValidator {
 
     // 회원 중복 검사
-    // 1. Mobile
     void duplicateCheck(Member member);
+
+    // 회원 오픈 예정 스터디 검사
+    static void openStudyCheck(Member member) {
+        // TODO
+    }
+
+    // 회원 스터디 소유 검사
+    static void studyOwnerCheck(Member member) {
+        // TODO
+    }
 }
