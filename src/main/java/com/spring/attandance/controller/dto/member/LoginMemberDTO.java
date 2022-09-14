e926b1c7b107adf56d4a2f18072eebeb6447a77f
June0619 <jjw5050@hotmail.com>
2022-09-14 17:28:28 +0900

    feat: LoginMemberDTO 연관관계 조회

--- src/main/java/com/spring/attandance/controller/dto/member/LoginMemberDTO.java
+++ src/main/java/com/spring/attandance/controller/dto/member/LoginMemberDTO.java
@@ -1,10 +1,17 @@
 package com.spring.attandance.controller.dto.member;
 
+import com.spring.attandance.domain.GroupMember;
 import com.spring.attandance.domain.Member;
+import com.spring.attandance.domain.enums.GroupRole;
 import lombok.Data;
 import lombok.Getter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
+@ToString
 public class LoginMemberDTO {
 
     private Long id;
@@ -12,20 +19,38 @@ public class LoginMemberDTO {
     private String mobile;
     private String email;
     private boolean isGuest;
+    private List<GroupMemberInnerDTO> groups = new ArrayList<>();
 
     public static LoginMemberDTO of(Member member) {
-        LoginMemberDTO loginMemberDTO = new LoginMemberDTO();
-        loginMemberDTO.id = member.getId();
-        loginMemberDTO.name = member.getName();
-        loginMemberDTO.mobile = member.getMobile();
-        loginMemberDTO.email = member.getEmail();
-        loginMemberDTO.isGuest = false;
-        return loginMemberDTO;
+        LoginMemberDTO dto = new LoginMemberDTO();
+        dto.id = member.getId();
+        dto.name = member.getName();
+        dto.mobile = member.getMobile();
+        dto.email = member.getEmail();
+        dto.isGuest = false;
+
+        for (GroupMember groupMember : member.getGroupMembers()) {
+            GroupMemberInnerDTO innerDTO = new GroupMemberInnerDTO();
+            innerDTO.setId(groupMember.getId());
+            innerDTO.setName(groupMember.getGroup().getName());
+            innerDTO.setRole(groupMember.getRole());
+            dto.groups.add(innerDTO);
+        }
+
+        return dto;
     }
 
     public LoginMemberDTO() {
         name = "Guest";
         isGuest = true;
+    }
+
+    @Data
+    @ToString
+    static class GroupMemberInnerDTO {
+        private Long id;
+        private String name;
+        private GroupRole role;
     }
 
 
