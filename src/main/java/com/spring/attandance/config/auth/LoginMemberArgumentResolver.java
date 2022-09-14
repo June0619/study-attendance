0c4996eb4eba841ee6c86f05e12b5ad297b6194d
June0619 <jjw5050@hotmail.com>
2022-09-14 17:26:27 +0900

    fix: methodArgumentResolver 컴포넌트 등록

--- src/main/java/com/spring/attandance/config/auth/LoginMemberArgumentResolver.java
+++ src/main/java/com/spring/attandance/config/auth/LoginMemberArgumentResolver.java
@@ -3,11 +3,13 @@ package com.spring.attandance.config.auth;
 import com.spring.attandance.context.MemberThreadLocal;
 import com.spring.attandance.controller.dto.member.LoginMemberDTO;
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Component
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
